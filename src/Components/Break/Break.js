db174f01b6ba56ef78737fe2d0b7211bc4ee57de
parthasuman <parthasuman123@gmail.com>
2022-09-29 21:19:15 +0600

    break updated

--- src/Components/Break/Break.js
+++ src/Components/Break/Break.js
@@ -3,7 +3,10 @@ import React from "react";
 const Break = (props) => {
   return (
     <div>
+      <button onclick={props.handleAddClick}>5</button>
       <button onclick={props.handleAddClick}>7</button>
+      <button onclick={props.handleAddClick}>10</button>
+      <button onclick={props.handleAddClick}>12</button>
     </div>
   );
 };
