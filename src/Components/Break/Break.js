11621d072c64ec52aee06f59a09d22f6279b90ec
parthasuman <parthasuman123@gmail.com>
2022-09-29 21:25:14 +0600

    break button added

--- src/Components/Break/Break.js
+++ src/Components/Break/Break.js
@@ -1,12 +1,21 @@
 import React from "react";
+import "./Break.css";
 
 const Break = (props) => {
   return (
-    <div className="break-btn">
-      <button onclick={props.handleAddClick}>5</button>
-      <button onclick={props.handleAddClick}>7</button>
-      <button onclick={props.handleAddClick}>10</button>
-      <button onclick={props.handleAddClick}>12</button>
+    <div>
+      <button className="break-btn1" onclick={props.handleAddClick}>
+        5
+      </button>
+      <button className="break-btn1" onclick={props.handleAddClick}>
+        7
+      </button>
+      <button className="break-btn1" onclick={props.handleAddClick}>
+        10
+      </button>
+      <button className="break-btn1" onclick={props.handleAddClick}>
+        12
+      </button>
     </div>
   );
 };
